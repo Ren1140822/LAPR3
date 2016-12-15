29e6dcc169482e7d896d037be4e2cfcf59448011
FlavioRelvas <1140826@isep.ipp.pt>
2016-12-15 17:43:03 +0000

    Creation of classes Flight, FlightInstance and RegularFlight

---
+++ src/main/java/lapr/project/model/Flight.java
@@ -0,0 +1,126 @@
+package lapr.project.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+/**
+ *
+ * @author Flavio Relvas
+ */
+public abstract class Flight implements Serializable {
+
+    /**
+     * Class attributes
+     */
+    String flightDesignator;
+    //private enum type{Regular, Charter}; //@TODO - create ENUM for this attribute
+    //private String departureData; //@TODO - ask what datatype this should be
+    int minStopTime; // time in minutes
+    Date scheduledArrival; //@TODO - should this be so sort of date
+
+    /**
+     * Default attributes
+     */
+    private final String DEFAULT_FLIGHT_DESIGNATOR = "No Designator";
+    private final int DEFAULT_MIN_STOP_TIME = 0;
+    private final Date DEFAULT_SCHEDULED_ARRIVAL = null;
+
+    /**
+     * Default constructor
+     */
+    public Flight() {
+        flightDesignator = DEFAULT_FLIGHT_DESIGNATOR;
+        minStopTime = DEFAULT_MIN_STOP_TIME;
+        scheduledArrival = DEFAULT_SCHEDULED_ARRIVAL;
+    }
+
+    /**
+     * Parameter constructor
+     *
+     * @param flightDesignator the flight designator
+     * @param minStopTime the minimun stop time for the flight
+     * @param scheduledArrival the scheduled date of arrival for the flight
+     */
+    public Flight(String flightDesignator, int minStopTime, Date scheduledArrival) {
+        this.flightDesignator = flightDesignator;
+        this.minStopTime = minStopTime;
+        this.scheduledArrival = scheduledArrival;
+    }
+
+    /**
+     * Copy constructor
+     *
+     * @param flight the flight to copy
+     */
+    public Flight(Flight flight) {
+        this.flightDesignator = flight.flightDesignator;
+        this.minStopTime = flight.minStopTime;
+        this.scheduledArrival = flight.scheduledArrival;
+    }
+
+    @Override
+    public String toString() {
+        return this.flightDesignator;
+    }
+
+    public boolean equals(Object otherObject) {
+        if (otherObject == null || this.getClass() != otherObject.getClass()) {
+            return false;
+        }
+        if (this == otherObject) {
+            return true;
+        }
+        Flight other = (Flight) otherObject;
+        return this.flightDesignator.equals(other.flightDesignator);
+    }
+
+    /**
+     * Gets the flight designator
+     * @return the flight's designator
+     */
+    public String getFlightDesignator() {
+        return flightDesignator;
+    }
+
+    /**
+     * Sets the flight designator
+     * @param flightDesignator the new flight designator
+     */
+    public void setFlightDesignator(String flightDesignator) {
+        this.flightDesignator = flightDesignator;
+    }
+
+    /**
+     * Gets the minimum stop time of the flight
+     * @return the minimum stop time of the flight
+     */
+    public int getMinStopTime() {
+        return minStopTime;
+    }
+
+    /**
+     * Sets the minimum stop time of the flight
+     * @param minStopTime the new minimun stop time for the flight
+     */
+    public void setMinStopTime(int minStopTime) {
+        this.minStopTime = minStopTime;
+    }
+
+    /**
+     * Gets the date of the scheduled arrival for the flight
+     * @return the date of the scheduled arrival of the flight
+     */
+    public Date getScheduledArrival() {
+        return scheduledArrival;
+    }
+
+    /**
+     * Sets the date of the scheduled arrival of the flight
+     * @param scheduledArrival the new date of the flight's arrival
+     */
+    public void setScheduledArrival(Date scheduledArrival) {
+        this.scheduledArrival = scheduledArrival;
+    }
+    
+    
+}
---
+++ src/main/java/lapr/project/model/RegularFlight.java
@@ -0,0 +1,72 @@
+package lapr.project.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Flavio Relvas
+ */
+public class RegularFlight extends Flight implements Serializable {
+
+    /**
+     * Class attributes
+     */
+    private List<Date> departureDays;
+
+    /**
+     * Default attributes
+     */
+    private final List<Date> DEFAULT_DEPARTURE_DAYS = new LinkedList<>();
+
+    /**
+     * Default constructor
+     */
+    public RegularFlight() {
+        super();
+        departureDays = DEFAULT_DEPARTURE_DAYS;
+    }
+
+    /**
+     * Parameter constructor
+     *
+     * @param flightDesignator the flight designator
+     * @param minStopTime the minimun stop time for the flight
+     * @param scheduledArrival the scheduled date of arrival for the flight
+     * @param departureDays the list of the days the flight actually occurs
+     */
+    public RegularFlight(String flightDesignator, int minStopTime, Date scheduledArrival, List<Date> departureDays) {
+        super(flightDesignator, minStopTime, scheduledArrival);
+        this.departureDays = departureDays;
+    }
+
+    /**
+     * Copy constructor
+     * @param flight the flight to copy
+     */
+    public RegularFlight(RegularFlight flight) {
+        super(flight);
+        this.departureDays = flight.departureDays;
+    }
+
+    /**
+     * Returns a string description of this object.
+     * @return the description of this object
+     */
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+     /**
+     * Checks if two object are equal.
+     * @param otherObject the other object
+     * @return true if equal
+     */
+    @Override
+    public boolean equals(Object otherObject) {
+        return super.equals(otherObject);
+    }
+}
