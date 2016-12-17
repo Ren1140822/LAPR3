a9c06303546c9811f7339cd5cc73a94edbdd0776
Pedro Fernandes <1060503@isep.ipp.pt>
2016-12-17 18:08:22 +0000

    general update

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -25,6 +25,6 @@ class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         ExportCSVUI ui = new ExportCSVUI();
+        MenuUI ui = new MenuUI();
     }
 }
