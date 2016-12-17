ae3d1c76fa950e0a4f3a34bc636536e06b5a3189
NANA <NANA@NANA-PC>
2016-12-17 02:29:41 +0000

    UC2-CreateProjectController

---
+++ src/main/java/lapr/project/controller/CreateProjectController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+/**
+ * Controller to create project
+ * @author Diana Silva
+ */
+public class CreateProjectController {
+    public void addProject(){
+        
+    }
+}
