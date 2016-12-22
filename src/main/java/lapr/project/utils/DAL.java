6d46e1cbf1b791c51f693499f11decfd304944f4
FlavioRelvas <1140826@isep.ipp.pt>
2016-12-22 16:31:16 +0000

    Created DAL class

---
+++ src/main/java/lapr/project/utils/DAL.java
@@ -0,0 +1,24 @@
+package lapr.project.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Flavio Relvas
+ */
+public class DAL {
+
+    private Connection con;
+    private final String url = "jdbc:oracle:thin:@gandalf.dei.isep.ipp.pt:1521/pdborcl";
+    private final String user = "LAPR3_38";
+    private final String password = "grupo38";
+
+    /*
+     try{
+     con = DriverManager.getConnection(url,user,pass);
+     }catch (SQLException e){
+     System.err.printf(e);
+     }*/
+    
+}
